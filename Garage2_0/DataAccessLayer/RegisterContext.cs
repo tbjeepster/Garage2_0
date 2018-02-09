cce55052b95f171d26d9e4dbb1e6f5c6e80df987
lenaholmstrom <User@DESK-GDZ7J32>
2018-02-09 15:25:41 +0100

    lh_partcommit

---
+++ Garage2_0/DataAccessLayer/RegisterContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Garage2_0.DataAccessLayer
+{
+    public class RegisterContext : DbContext
+    {
+
+        public RegisterContext() : base("DefaultConnection")
+        {
+
+        }
+
+        //public DbSet<Models.ParkedVehicle> Vehicle { get; set; }
+    }
+}
