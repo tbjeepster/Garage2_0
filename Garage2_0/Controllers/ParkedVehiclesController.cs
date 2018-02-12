c4010f2c99e3ea7e0bb94d9f5afe921808ffcff5
Hans Davidsson <error@nodomain.local>
2018-02-12 10:19:15 +0100

    Controller and view skeletons added for ParkedVehicles.

---
+++ Garage2_0/Controllers/ParkedVehiclesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Garage2_0.DataAccessLayer;
+using Garage2_0.Models;
+
+namespace Garage2_0.Controllers
+{
+    public class ParkedVehiclesController : Controller
+    {
+        private RegisterContext db = new RegisterContext();
+
+        // GET: ParkedVehicles
+        public ActionResult Index()
+        {
+            return View(db.Vehicle.ToList());
+        }
+
+        // GET: ParkedVehicles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkedVehicle parkedVehicle = db.Vehicle.Find(id);
+            if (parkedVehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkedVehicle);
+        }
+
+        // GET: ParkedVehicles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParkedVehicles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Type,RegNum,Colour,ParkedTime,NumOfWeels,CarMake,Model")] ParkedVehicle parkedVehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vehicle.Add(parkedVehicle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(parkedVehicle);
+        }
+
+        // GET: ParkedVehicles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkedVehicle parkedVehicle = db.Vehicle.Find(id);
+            if (parkedVehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkedVehicle);
+        }
+
+        // POST: ParkedVehicles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Type,RegNum,Colour,ParkedTime,NumOfWeels,CarMake,Model")] ParkedVehicle parkedVehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(parkedVehicle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(parkedVehicle);
+        }
+
+        // GET: ParkedVehicles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkedVehicle parkedVehicle = db.Vehicle.Find(id);
+            if (parkedVehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkedVehicle);
+        }
+
+        // POST: ParkedVehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ParkedVehicle parkedVehicle = db.Vehicle.Find(id);
+            db.Vehicle.Remove(parkedVehicle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
