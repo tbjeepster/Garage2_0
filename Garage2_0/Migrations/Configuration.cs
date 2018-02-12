92a2d1bd6adae591c8a386c091120c75715a98e7
lenaholmstrom <User@DESK-GDZ7J32>
2018-02-12 15:37:50 +0100

    Removed Homecontroller and views

--- Garage2_0/Migrations/Configuration.cs
+++ Garage2_0/Migrations/Configuration.cs
@@ -17,23 +17,23 @@ namespace Garage2_0.Migrations
         {
             context.Vehicle.AddOrUpdate(
                    p => p.RegNum,
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "ASD453", Colour = "Röd", CarMake = "Volvo", Model = "Kaa", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "SUV", RegNum = "ASD953", Colour = "Svart", CarMake = "Ford", Model = "F10", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "BSD451", Colour = "Vit", CarMake = "Ford", Model = "Mondeo", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "CSD452", Colour = "Silver", CarMake = "Opel", Model = "Kadett", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "DSD453", Colour = "Vit", CarMake = "Toyota", Model = "Avensis", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "XSD453", Colour = "Blå", CarMake = "Mazda", Model = "323", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "ZSD453", Colour = "Gul", CarMake = "Maserati", Model = "Mistral", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "SUV", RegNum = "XXX453", Colour = "Orange", CarMake = "Dodge", Model = "Ram", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "SUV", RegNum = "AJK453", Colour = "Svart", CarMake = "Jeep", Model = "CJ5", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "SUV", RegNum = "ATR453", Colour = "Svart", CarMake = "Jeep", Model = "Cherokee", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "BBD453", Colour = "Guld", CarMake = "Audi", Model = "A5", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "ASDR122", Colour = "Vit", CarMake = "Mercedes", Model = "PV", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "ASD477", Colour = "Vit", CarMake = "Mercedes", Model = "PV", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "AST433", Colour = "Grön", CarMake = "Saab", Model = "9000", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "ATY417", Colour = "Grå", CarMake = "Renault", Model = "Kangoo", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "ASU481", Colour = "Röd", CarMake = "Ford", Model = "Scorpio", NumOfWeels = 4 },
-                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = "Personbil", RegNum = "ASR408", Colour = "Orange", CarMake = "Alfa-Romeo", Model = "Montreal", NumOfWeels = 4 });
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "ASD453", Colour = "Röd", CarMake = "Volvo", Model = "Kaa", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.SUV, RegNum = "ASD953", Colour = "Svart", CarMake = "Ford", Model = "F10", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "BSD451", Colour = "Vit", CarMake = "Ford", Model = "Mondeo", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "CSD452", Colour = "Silver", CarMake = "Opel", Model = "Kadett", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "DSD453", Colour = "Vit", CarMake = "Toyota", Model = "Avensis", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "XSD453", Colour = "Blå", CarMake = "Mazda", Model = "323", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "ZSD453", Colour = "Gul", CarMake = "Maserati", Model = "Mistral", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.SUV, RegNum = "XXX453", Colour = "Orange", CarMake = "Dodge", Model = "Ram", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.SUV, RegNum = "AJK453", Colour = "Svart", CarMake = "Jeep", Model = "CJ5", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.SUV, RegNum = "ATR453", Colour = "Svart", CarMake = "Jeep", Model = "Cherokee", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "BBD453", Colour = "Guld", CarMake = "Audi", Model = "A5", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "ASDR122", Colour = "Vit", CarMake = "Mercedes", Model = "PV", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "ASD477", Colour = "Vit", CarMake = "Mercedes", Model = "PV", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "AST433", Colour = "Grön", CarMake = "Saab", Model = "9000", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "ATY417", Colour = "Grå", CarMake = "Renault", Model = "Kangoo", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "ASU481", Colour = "Röd", CarMake = "Ford", Model = "Scorpio", NumOfWeels = 4 },
+                   new ParkedVehicle {ParkedTime= DateTime.Now , Type = ParkedVehicle.VehicleType.Personbil, RegNum = "ASR408", Colour = "Orange", CarMake = "Alfa-Romeo", Model = "Montreal", NumOfWeels = 4 });
         }
     }
 }
