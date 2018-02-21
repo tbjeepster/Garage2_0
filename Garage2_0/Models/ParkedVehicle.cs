e28b21ac6afd3a9bab77adf1af22e6bdee44e767
lenaholmstrom <User@DESK-GDZ7J32>
2018-02-21 09:40:49 +0100

    lh added functionality for drop

--- Garage2_0/Models/ParkedVehicle.cs
+++ Garage2_0/Models/ParkedVehicle.cs
@@ -39,97 +39,26 @@ namespace Garage2_0.Models
             [StringLength(30, MinimumLength = 1, ErrorMessage = "Längd måste tillhöra intervallet [1, 30]")]
             [DisplayName("Modell")]
             public string Model { get; set; }
-
+       
             public virtual Member Member { get; set; }
-            public virtual VehicleType Type { get; set; }
+            public virtual VehicleType Type { get; set;}
     }
-
     //Indexview
     public class ParkedVehicleProjection01
     {
-        public int Id { get; set; }
+        public int                       Id         { get; set; }
 
-        public int TypeId { get; set; }
-
-        [Required]
-        [DisplayName("Medlemsnummer")]
-        public int MemberId { get; set; }
 
         [DisplayName("RegNum")]
-        public string RegNum { get; set; }
+        public string                    RegNum     { get; set; }
 
         [DisplayName("Ankomsttid")]
-        public DateTime ParkedTime { get; set; }
+        public DateTime                  ParkedTime { get; set; }
 
         [DisplayName("Märke")]
-        public string CarMake { get; set; }
+        public string                    CarMake    { get; set; }
 
         [DisplayName("Modell")]
-        public string Model { get; set; }
+        public string                    Model      { get; set; }
     }
-
-    public class ParkedVehicleProjection01Ext01
-    {
-        public int Id { get; set; }
-
-        public int TypeId { get; set; }
-
-        [DisplayName("Fordonstyp")]
-        public string TypeName { get; set; }
-
-        [Required]
-        [DisplayName("Medlemsnummer")]
-        public int MemberId { get; set; }
-        [Required]
-        [DisplayName("Medlem")]
-        public string MemberName { get; set; }
-
-        [DisplayName("RegNum")]
-        public string RegNum { get; set; }
-
-        [DisplayName("Ankomsttid")]
-        public DateTime ParkedTime { get; set; }
-
-        [DisplayName("Märke")]
-        public string CarMake { get; set; }
-
-        [DisplayName("Modell")]
-        public string Model { get; set; }
-    }
-
-    public class ParkedVehicleExt01
-    {
-        public int Id { get; set; }
-
-        public int TypeId { get; set; }
-
-        [DisplayName("Fordonstyp")]
-        public string TypeName { get; set; }
-
-        [Required]
-        [DisplayName("Medlemsnummer")]
-        public int MemberId { get; set; }
-        [Required]
-        [DisplayName("Medlem")]
-        public string MemberName { get; set; }
-
-        [DisplayName("Färg")]
-        public string Colour { get; set; }
-
-        [DisplayName("Hjulantal")]
-        public int NumOfWeels { get; set; }
-
-        [DisplayName("RegNum")]
-        public string RegNum { get; set; }
-
-        [DisplayName("Ankomsttid")]
-        public DateTime ParkedTime { get; set; }
-
-        [DisplayName("Märke")]
-        public string CarMake { get; set; }
-
-        [DisplayName("Modell")]
-        public string Model { get; set; }
-    }
-
 }
